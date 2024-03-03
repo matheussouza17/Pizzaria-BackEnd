ebd7142fb007fffff43210c75f0087b6f6b85ee5
Matheus Souza <mathheus.souza@outlook.com>
2024-03-03 10:26:08 -0300

    Throw error

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Request, Router, Response } from "express";
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
+    //throw new Error('Throw message of error!')
     return res.json({ok : true})
 })
 
